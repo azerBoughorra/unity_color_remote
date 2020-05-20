b329cfeb27e7b4adf75778b5bddd995925d7888a
Azer Boughorra <azer.boughorra@gmail.com>
2020-05-20 01:45:34 +0100

    first commit

---
+++ Assets/Scripts/playermovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    Animator anim;
+    public float speed;
+    
+    Rigidbody2D rb;
+    public int movementspeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        HandleMovement(horizontal);
+        
+    }
+    void HandleMovement(float horizonal)
+    {
+        //CARACTER MOVEMENT
+  
+        rb.velocity = new Vector2(horizonal * movementspeed, rb.velocity.y);
+        if (horizonal != 0)
+        {
+            if (horizonal < 0)
+            {
+                transform.localScale = new Vector2(-1,1);
+            }
+            else
+            {
+                transform.localScale = new Vector2(1,1);
+            }
+                
+            
+        }
+       if(Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("walk", true);
+        }
+       else
+        anim.SetBool("walk", false);
+    }
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("walk", true);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("inair", true);
+        }
+        else
+        {
+            anim.SetBool("inair", false);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetBool("punsh", true);
+        }
+        else
+        {
+            anim.SetBool("punsh", false);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetBool("run", true);
+        }
+        else
+        {
+            anim.SetBool("run", false);
+        }
+
+    }
+}
